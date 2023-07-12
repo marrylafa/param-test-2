b23232e05d4034a6d66a9792852a9965804b5e85
Mariia Chorna <marrylafa@gmail.com>
2023-07-12 21:59:01 +0300

    homework

---
+++ cypress/e2e/param.cy.js
@@ -0,0 +1,37 @@
+
+  
+describe('Parametrized test', ()=>{
+
+  beforeEach(() => {
+    
+    cy.viewport(1800, 1000);
+  
+  })
+
+  const tests = [['john@mail.com', 'john1993',]];
+
+  tests.forEach(text => {
+    it(`Input checking "${text}"`, () => { 
+      cy.visit('https://sanitarskyi-ngx-admin.herokuapp.com');
+      cy.get('[src="assets/images/material-dark-theme.jpg"]').click();
+      cy.get('[title="Forms"]').click();
+      cy.get('[title="Form Layouts"]').click();
+
+      
+      cy.get('nb-card nb-card-header').contains( 'Basic form').parent()
+      .find('input[placeholder="Email"]').type(text[0])
+
+      cy.get('nb-card nb-card-header').contains( 'Basic form').parent()
+      .find('input[placeholder="Password"]').type(text[1])
+
+      cy.get('nb-card nb-card-header').contains('Basic form').parent()
+      .find('span.custom-checkbox').click()
+
+      cy.get('nb-card nb-card-header').contains('Basic form').parent()
+      .find('button.appearance-filled').click();
+
+
+    })
+  })
+
+})  
